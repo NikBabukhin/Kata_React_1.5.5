8015219072a16666fda06de16e1139eff20e90a1
Бабухин Николай Вячеславович <nikolai.babukhin@rcntec.com>
2024-02-02 14:27:28 +0300

    Fix second chance

--- husky.config.js
+++
@@ -1,8 +0,0 @@
-const runYarnLock = 'yarn install --frozen-lockfile';
-
-export const hooks = {
-  'post-checkout': `if [[ $HUSKY_GIT_PARAMS =~ 1$ ]]; then ${runYarnLock}; fi`,
-  'post-merge': runYarnLock,
-  'post-rebase': 'yarn install',
-  'pre-commit': 'lint-staged'
-};
